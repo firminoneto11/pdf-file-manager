405376b11eaf0741c7ae16501a598946bffd48de
Firmino Neto <firminoneto11@hotmail.com>
2021-04-09 18:30:22 -0300

    Segundo dia do projeto. Implementei o menu inicial contendo 3 botoes que irao acionar as acoes principais do programa. Esses botoes tambem foram estilizados e uma paletta de cores de tom azul foi escolhida para o projeto.

---
+++ interfaces/main_menu_gui.py
@@ -0,0 +1,61 @@
+"""
+A cor '#29274C' está fora da paleta de cores, mas é um tom de azul que caiu bem com a tonalidade do projeto.
+"""
+from tkinter import *
+
+
+class MenuInicial:
+
+    def __init__(self, root):
+        """
+        Essa classe inicializa o menu inicial logo após o carregamento da barra de progresso.
+        :param root: Janela raiz.
+        """
+        # Colocando a janela principal em um atributo de instância
+        self.root = root
+
+        # Criando os widgets iniciais
+        self.status_do_sistema = Label(root, text='Selecione uma das opções abaixo 👇', font=('Helvetica', 18),
+                                       bg='#624CAB', fg='#eeeeee')
+
+        self.concatenar = Button(root, text='Concatenar', width=20, bg='#29274C', fg='#eeeeee', relief='flat',
+                                 font=('Helvetica', 12), padx=5, pady=5, borderwidth=2, activebackground='#7189FF')
+        self.separar = Button(root, text='Separar', width=20, bg='#29274C', fg='#eeeeee', relief='flat',
+                              font=('Helvetica', 12), padx=5, pady=5, borderwidth=2, activebackground='#7189FF')
+
+        self.sair = Button(root, text='Sair', width=20, bg='#29274C', fg='#eeeeee', relief='flat',
+                           font=('Helvetica', 12), padx=5, pady=5, borderwidth=2, activebackground='#7189FF',
+                           command=self.root.quit)
+
+        # Inserindo os elementos na tela
+        self.status_do_sistema.place(x=250, y=150)
+
+        self.concatenar.place(x=250, y=250)
+        self.separar.place(x=500, y=250)
+        self.sair.place(x=375, y=330)
+
+        # Criando os efeitos hovers dos botões
+        self.concatenar.bind('<Enter>', self.__1_hover_in)
+        self.concatenar.bind('<Leave>', self.__1_hover_out)
+        self.separar.bind('<Enter>', self.__2_hover_in)
+        self.separar.bind('<Leave>', self.__2_hover_out)
+        self.sair.bind('<Enter>', self.__3_hover_in)
+        self.sair.bind('<Leave>', self.__3_hover_out)
+
+    def __1_hover_in(self, event):
+        self.concatenar.config(bg='#758ECD')
+
+    def __1_hover_out(self, event):
+        self.concatenar.config(bg='#29274C')
+
+    def __2_hover_in(self, event):
+        self.separar.config(bg='#758ECD')
+
+    def __2_hover_out(self, event):
+        self.separar.config(bg='#29274C')
+
+    def __3_hover_in(self, event):
+        self.sair.config(bg='#758ECD')
+
+    def __3_hover_out(self, event):
+        self.sair.config(bg='#29274C')
--- main_foo.py
+++ main_foo.py
@@ -4,20 +4,7 @@ from tkinter import ttk
 from time import sleep
 from functions.geometria_centralizada import centralizar_janela
 from functions.verificar_diretorios import verifica_dirs
-
-
-def progresso(pb, lab):
-    """
-    Essa função realiza a inserção na tela da barra de progresso e animação da mesma.
-    :param pb: Objeto do tipo Progressbar
-    :param lab: Objeto do tipo Label
-    :return: None
-    """
-    for _ in range(100):
-        pb['value'] = pb['value'] + 1
-        lab.config(text=f"{pb['value']}%")
-        pb.update_idletasks()
-        sleep(0.015)
+from interfaces.main_menu_gui import MenuInicial
 
 
 def main():
@@ -32,7 +19,7 @@ def main():
         # Aplicando configurações a janela
         janela_raiz.title("PDF file manager by Firmino Neto")
         janela_raiz.iconbitmap(r".\icon.ico")
-        janela_raiz.configure(background='#7E52A0')
+        janela_raiz.configure(background='#624CAB')
         janela_raiz.resizable(False, False)
         centralizar_janela(width=1000, height=500, element=janela_raiz)
 
@@ -40,27 +27,41 @@ def main():
         verifica_dirs()
 
         # Criando os widgets que irão apresentar uma barra de carregamento
-        status_do_carregamento = Label(janela_raiz, text='0%', font=('Helvetica', 14), bg='#7E52A0', fg='#eeeeee')
+        status_do_carregamento = Label(janela_raiz, text='0%', font=('Helvetica', 14), bg='#624CAB', fg='#eeeeee')
 
         estilo = ttk.Style()
-        estilo.theme_use('clam')
-        estilo.configure("aqua.Horizontal.TProgressbar", foreground='white', background='aqua')
+        estilo.theme_use('alt')
+        estilo.configure("aqua.Horizontal.TProgressbar", background='aqua')
+
         progress_bar = ttk.Progressbar(janela_raiz, style="aqua.Horizontal.TProgressbar", orient=HORIZONTAL, length=500,
                                        mode='determinate')
 
-        loading = Label(janela_raiz, text='Carregando...', font=('Helvetica', 14), bg='#7E52A0', fg='#eeeeee')
+        loading = Label(janela_raiz, text='', font=('Helvetica', 16), bg='#624CAB', fg='#eeeeee')
 
         status_do_carregamento.place(x=480, y=300)
         progress_bar.place(x=250, y=250)
         loading.place(x=250, y=200)
 
-        progresso(pb=progress_bar, lab=status_do_carregamento)
+        # Loop 'for' para efetuar o carregamento da barra de progresso
+        for _ in range(100):
+            if progress_bar['value'] >= 0 and (progress_bar['value'] < 50):
+                loading.config(text='Carregando...')
+            else:
+                loading.config(text='Checando diretórios...')
 
-        if progress_bar['value'] == 100:
-            progress_bar.destroy()
-            status_do_carregamento.destroy()
-            loading.destroy()
-            Label(janela_raiz, text='Carregado!', font=('Helvetica', 16), bg='#7E52A0', fg='#eeeeee').pack(pady=100)
+            progress_bar['value'] = progress_bar['value'] + 1
+            status_do_carregamento.config(text=f"{progress_bar['value']}%")
+            progress_bar.update_idletasks()
+            sleep(0.020)
+
+        # Após a execução da função que realiza o carregamento da barra de progresso, os widgets são removidos e o menu
+        # inicial é apresentado
+        progress_bar.destroy()
+        status_do_carregamento.destroy()
+        loading.destroy()
+
+        # Inicializando a classe MenuInicial
+        MenuInicial(root=janela_raiz)
 
         # Mainloop da aplicação
         janela_raiz.mainloop()
